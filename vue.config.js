77ca4ce41302f5d51bcb520bf1210bc6e13f85a9
Rookie <gb880327@189.cn>
2019-04-13 21:33:24 +0800

    添加APP图标

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,18 @@ module.exports = {
             externals: ['ejs'],
             // If you are using Yarn Workspaces, you may have multiple node_modules folders
             // List them all here so that VCP Electron Builder can find them
-            nodeModulesPath: ['./node_modules']
+            nodeModulesPath: ['./node_modules'],
+            builderOptions: {
+                mac: {
+                    target: 'dmg',
+                    icon: 'build/icons/logo.icns',
+                    type: 'distribution'
+                },
+                win: {
+                    target: 'zip',
+                    icon: 'build/icons/logo.ico'
+                }
+            }
         }
     }
 }
