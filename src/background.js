8370acdfb0f3c77bff08a49cd3fea0e394590262
Rookie <gb880327@189.cn>
2019-04-18 09:14:58 +0800

    添加菜单

--- src/background.js
+++ src/background.js
@@ -1,6 +1,5 @@
 'use strict'
-
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, Menu } from 'electron'
 import {
     createProtocol,
     installVueDevtools
@@ -13,6 +12,89 @@ let win
 
 // Standard scheme must be registered before the app is ready
 protocol.registerStandardSchemes(['app'], { secure: true })
+
+
+const template = [{
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' }, // !! 这里加入打开调试工具, 如果你不希望打开请去掉这行
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'close' }
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [{
+            label: 'Learn More',
+            click() { require('electron').shell.openExternal('https://electronjs.org') }
+        }]
+    }
+]
+
+if (process.platform === 'darwin') {
+    template.unshift({
+        label: app.getName(),
+        submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services', submenu: [] },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+        ]
+    })
+
+    // Edit menu
+    template[1].submenu.push({ type: 'separator' }, {
+        label: 'Speech',
+        submenu: [
+            { role: 'startspeaking' },
+            { role: 'stopspeaking' }
+        ]
+    })
+
+    // Window menu
+    template[3].submenu = [
+        { role: 'close' },
+        { role: 'minimize' },
+        { role: 'zoom' },
+        { type: 'separator' },
+        { role: 'front' }
+    ]
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 function createWindow() {
     const electronScreen = require('electron').screen
